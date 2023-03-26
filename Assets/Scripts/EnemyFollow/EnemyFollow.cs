8dfa797177628912cf6cfecd0c1ea0466a007472
SaguarSesame <103561075+SaguarSesame@users.noreply.github.com>
2023-03-26 19:52:47 +0300

    room4.1

--- Assets/Scripts/EnemyFollow/EnemyFollow.cs
+++ Assets/Scripts/EnemyFollow/EnemyFollow.cs
@@ -9,18 +9,18 @@ public class EnemyFollow : MonoBehaviour
     [SerializeField] PlayerMovement2Dmodified MovementScript;
    // public Transform Player;
     List<Vector2> position;
+    bool executa = false;
     void Start()
     {
-        position = new List<Vector2>();  
+        position = new List<Vector2>();
     }
 
     private void Update()
     {
-        if(MovementScript.movement != Vector2.zero)
+        if(MovementScript.movement != Vector2.zero && position.Count < 51)
             position.Add(MovementScript.rb.position);
-        if(position.Count > 50)
+        if (position.Count > 50)
             InvokeRepeating("Following", 0.0f, 1.0f);
-        
     }
 
     void Following()
@@ -29,6 +29,8 @@ public class EnemyFollow : MonoBehaviour
         {
             transform.position = position[0];
             position.RemoveAt(0);
+            if (MovementScript.movement != Vector2.zero)
+                position.Add(MovementScript.rb.position);
             Debug.Log(position[0].x + " " + position[0].y);
         }
 
