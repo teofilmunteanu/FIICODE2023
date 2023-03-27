5dff6b3a7ffe19b7538e22f758c5a064e97ab83f
Pflorin11 <florin11_puscasu@yahoo.com>
2023-03-28 01:40:03 +0300

    maze gen v2

--- Assets/Maze/LevelGen.cs
+++ Assets/Maze/LevelGen.cs
@@ -32,29 +32,45 @@ public class LevelGen : MonoBehaviour
         mazes.Add(newMaze);
         ocupied += dimensionX * dimensionY;
 
-
+        bool reset=false;
         while (mazeSize.x*mazeSize.y>ocupied)
         {
 
             Debug.Log("generam maze nr " + mazeCount);
 
-            dimensionX = Random.Range(4, mazeSize.x / 4);
-            dimensionY = Random.Range(4, mazeSize.y / 4);
+            dimensionX = Random.Range(4, mazeSize.x / 2);
+            dimensionY = Random.Range(4, mazeSize.y / 2);
 
 
             ocupied += dimensionX * dimensionY;
+            Vector3 v3 = new Vector3();
+            if(reset)
+            {
+                v3 = mazepos;
+                reset = false;
+            }
+            else
+            {
+                v3 = mazes[mazeCount - 1].getCenter() + new Vector3(mazes[mazeCount - 1].getMazeSize().x / 2f + dimensionX / 2f, 0, 0);
+            }
 
-            MazeGen newMaze2 = Instantiate(genPrefab, mazes[mazeCount-1].getCenter() + new Vector3(mazes[mazeCount-1].getMazeSize().x / 2f + dimensionX / 2f, 0, 0), Quaternion.identity, transform);
-            newMaze2.setCenter(mazes[mazeCount - 1].getCenter() + new Vector3(mazes[mazeCount - 1].getMazeSize().x / 2f + dimensionX / 2f,0,0));
+            MazeGen newMaze2 = Instantiate(genPrefab, v3, Quaternion.identity, transform);
+            newMaze2.setCenter(v3);
             newMaze2.setMazeSize(dimensionX, dimensionY);
             mazes.Add(newMaze2);
+
+            if(newMaze2.transform.position.x+dimensionX>mazeSize.x)
+            {
+                mazepos = mazepos + new Vector3(0, 0, 10);
+                reset = true;
+            }
+
             mazeCount++;
 
 
         }
-        
 
-        foreach(MazeGen m in mazes)
+        foreach (MazeGen m in mazes)
         {
             m.Generate(Instant);
         }
