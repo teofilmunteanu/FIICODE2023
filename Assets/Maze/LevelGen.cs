3cf71a31c7d443a57f9e85146c7452b94cea530f
Pflorin11 <florin11_puscasu@yahoo.com>
2023-03-28 22:31:46 +0300

    push maze lipit

--- Assets/Maze/LevelGen.cs
+++ Assets/Maze/LevelGen.cs
@@ -9,23 +9,40 @@ public class LevelGen : MonoBehaviour
     [SerializeField] Vector2Int mazeSize;
     [SerializeField] bool Instant;
 
-
     // Start is called before the first frame update
     void Start()
     {
+        generateLines();
+
+
+        
+
+    }
+
+    void generatesquare()
+    {
+
+        int[,] m = new int[mazeSize.x,mazeSize.y];
+
+        for (int i = 0; i < mazeSize.x; i++)
+        {
+            for (int j = 0; j < mazeSize.y; j++)
+            {
+                m[i, j] = 0;
+            }
+        }
+
         Vector3 mazepos = new Vector3(0, 0, 0) + transform.GetComponentInParent<Transform>().position;
-        Vector3 maxX = mazepos;
-        int ocupied=0;
 
         List<MazeGen> mazes = new List<MazeGen>();
 
-        int maxRangeX = (mazeSize.x /2) + 1;
-        int maxRangeY = (mazeSize.y / 2) + 1;
+        int maxRangeX = (mazeSize.x / 2) + 1;
+        //int maxRangeY = (mazeSize.y / 2) + 1;
 
+        int dimensionX = Random.Range(4, maxRangeX);
+        int dimensionY = 6;//Random.Range(4, maxRangeY);
 
-        int dimensionX = Random.Range(5, maxRangeX);
-        int dimensionY = Random.Range(5, maxRangeY);
-
+        int ocupied = 0;
 
         MazeGen newMaze = Instantiate(genPrefab, mazepos, Quaternion.identity, transform);
         newMaze.setCenter(mazepos);
@@ -34,68 +51,37 @@ public class LevelGen : MonoBehaviour
         mazes.Add(newMaze);
         ocupied += dimensionX * dimensionY;
 
-        bool reset=false;
-        while (mazeSize.x*mazeSize.y>ocupied)
+        for(int i=0;i<dimensionX;i++)
+        {
+            for (int j = 0; j < dimensionY; j++)
+            {
+                m[i,j] = 1;
+            }
+        }
+
+        while (mazeSize.x * mazeSize.y > ocupied)
         {
 
-            //Debug.Log("generam maze nr " + mazeCount);
 
-            dimensionX = Random.Range(4, maxRangeX);
-            dimensionY = Random.Range(4, maxRangeY);
-
-
-            ocupied += dimensionX * dimensionY;
-            Vector3 v3 = new Vector3();
-            if(reset)
-            {
-                v3 = mazepos;
-                reset = false;
-            }
-            else
-            {
-                v3 = mazes[mazeCount - 1].getCenter() + new Vector3(mazes[mazeCount - 1].getMazeSize().x / 2f + dimensionX / 2f, 0, 0);
-            }
-
-            MazeGen newMaze2 = Instantiate(genPrefab, v3, Quaternion.identity, transform);
-            newMaze2.setCenter(v3);
-            newMaze2.setMazeSize(dimensionX, dimensionY);
-            mazes.Add(newMaze2);
-
-            if(newMaze2.transform.position.x+dimensionX>mazeSize.x)
-            {
-                mazepos = mazepos + new Vector3(0, 0, maxRangeY-1);
-                reset = true;
-            }
-
-            mazeCount++;
+            
 
 
         }
-
-        foreach (MazeGen m in mazes)
-        {
-            m.Generate(Instant);
-        }
-
-
     }
-
 
     void generateLines()
     {
-
         Vector3 mazepos = new Vector3(0, 0, 0) + transform.GetComponentInParent<Transform>().position;
-        Vector3 maxX = mazepos;
         int ocupied = 0;
 
         List<MazeGen> mazes = new List<MazeGen>();
 
-        int maxRangeX = (mazeSize.x / 3) + 1;
-        int maxRangeY = (mazeSize.y / 3) + 1;
+        int maxRangeX = (mazeSize.x / 2) + 1;
+        //int maxRangeY = (mazeSize.y / 2) + 1;
 
 
-        int dimensionX = Random.Range(5, maxRangeX);
-        int dimensionY = Random.Range(5, maxRangeY);
+        int dimensionX = Random.Range(4, maxRangeX);
+        int dimensionY = 6;//Random.Range(5, maxRangeY);
 
 
         MazeGen newMaze = Instantiate(genPrefab, mazepos, Quaternion.identity, transform);
@@ -106,19 +92,22 @@ public class LevelGen : MonoBehaviour
         ocupied += dimensionX * dimensionY;
 
         bool reset = false;
-        while (mazeSize.x * mazeSize.y > ocupied)
+
+        int rows = 0;
+        while (rows<4 || mazeSize.x * mazeSize.y > ocupied||!reset)
         {
 
             //Debug.Log("generam maze nr " + mazeCount);
 
             dimensionX = Random.Range(4, maxRangeX);
-            dimensionY = Random.Range(4, maxRangeY);
+            //dimensionY = Random.Range(4, maxRangeY);
 
 
             ocupied += dimensionX * dimensionY;
-            Vector3 v3 = new Vector3();
+            Vector3 v3;
             if (reset)
             {
+                maxRangeX = (mazeSize.x / 2) + 1;
                 v3 = mazepos;
                 reset = false;
             }
@@ -134,8 +123,9 @@ public class LevelGen : MonoBehaviour
 
             if (newMaze2.transform.position.x + dimensionX > mazeSize.x)
             {
-                mazepos = mazepos + new Vector3(0, 0, maxRangeY - 1);
+                mazepos = mazepos + new Vector3(0, 0, 6);
                 reset = true;
+                rows++;
             }
 
             mazeCount++;
@@ -147,6 +137,7 @@ public class LevelGen : MonoBehaviour
         {
             m.Generate(Instant);
         }
+
     }
 
 }
